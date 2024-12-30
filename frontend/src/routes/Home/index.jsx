f6de6954c2e2ab047c2862f042909844aa3cc5b3
IveryChen <ivy_chen1@brown.edu>
2024-12-30 10:55:01 -0800

    add StyledJar

--- frontend/src/routes/Home/index.jsx
+++ frontend/src/routes/Home/index.jsx
@@ -1,10 +1,37 @@
+import styled from "@emotion/styled";
 import React from "react";
 
+import Box from "../../components/Box";
 import { withRouter } from "../../utils/withRouter";
 
+const StyledJar = styled(Box)`
+  height: 320px;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    height: auto;
+    width: 90%;
+  }
+`;
+
 class Home extends React.PureComponent {
+  ref = React.createRef();
+  threeInstance = null;
+
+  componentDidMount() {
+    //   if (this.ref.current) {
+    //     this.threeInstance = initThreeJS(this.ref.current);
+    //   }
+  }
+
+  componentWillUnmount() {
+    //   if (this.threeInstance?.cleanup) {
+    //     this.threeInstance.cleanup();
+    //   }
+  }
+
   render() {
-    return null;
+    return <StyledJar as="canvas" justifySelf="center" ref={this.ref} />;
   }
 }
 
