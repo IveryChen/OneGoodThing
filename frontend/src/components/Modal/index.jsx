baccf593228935bde3947da0681ec9a4f116f76e
IveryChen <ivy_chen1@brown.edu>
2024-12-30 08:29:59 -0800

    add Modal component

---
+++ frontend/src/components/Modal/index.jsx
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+import Box from "../../components/Box";
+
+const StyledBox = styled(Box)`
+  backdrop-filter: blur(4px);
+`;
+
+export default class Modal extends React.PureComponent {
+  render() {
+    const { isOpen, onClose, children } = this.props;
+
+    if (!isOpen) return null;
+
+    return (
+      <StyledBox
+        alignItems="center"
+        bottom={0}
+        display="flex"
+        justifyContent="center"
+        left={0}
+        onClick={onClose}
+        position="fixed"
+        right={0}
+        top={0}
+      >
+        <Box
+          bg="lightgray"
+          borderRadius="2px"
+          borderStyle="solid"
+          borderWidth={1}
+          maxHeight="80%"
+          onClick={(e) => e.stopPropagation()}
+          overflow="auto"
+          p="16px"
+          width="90%"
+        >
+          {children}
+        </Box>
+      </StyledBox>
+    );
+  }
+}
