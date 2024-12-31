ea545daf88fd9849e9bd57dd4c748de5ccec2016
IveryChen <ivy_chen1@brown.edu>
2024-12-31 11:14:17 -0800

    modify onClick to sign in

--- frontend/src/routes/Lobby/index.jsx
+++ frontend/src/routes/Lobby/index.jsx
@@ -5,7 +5,13 @@ import { withRouter } from "../../utils/withRouter";
 
 class Lobby extends React.PureComponent {
   onClick = () => {
-    this.props.navigate(`/home`);
+    const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+    const redirectUri = import.meta.env.VITE_REDIRECT_URL;
+    const scope = "email profile";
+
+    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
+
+    window.location.href = googleAuthUrl;
   };
 
   render() {
