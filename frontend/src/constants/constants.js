b5cccae3bd5efee58d391777d62a204b3c71cf4a
IveryChen <ivy_chen1@brown.edu>
2024-12-30 21:14:24 -0800

    add stickies colours

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,5 +1,13 @@
 export const theme = {
+  blue: "#7afcff",
   darkgray: "#414141",
   lightgray: "#737373",
+};
+
+export const stickies = {
+  lightpink: "#ff7eb9",
+  lightyellow: "#feff9c",
   oatmeal: "#e7e5e3",
+  pink: "#ff65a3",
+  yellow: "#fff740",
 };
