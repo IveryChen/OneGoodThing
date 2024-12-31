2103279eb8b27024a3b64bf2da08740a42aa0a72
IveryChen <ivy_chen1@brown.edu>
2024-12-30 20:54:35 -0800

    add styling to header

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { MdAdd } from "react-icons/md";
 
 import Box from "../../components/Box";
+import { theme } from "../../constants/constants";
 
 export default class Header extends React.PureComponent {
   render() {
-    return <Box height={56} bg="red" />;
+    return (
+      <Box height={56} p="8px">
+        <Box
+          bg="white"
+          borderColor={theme.darkgray}
+          borderRadius="50%"
+          borderStyle="solid"
+          borderWidth={2}
+          display="grid"
+          justifyContent="center"
+          justifySelf="end"
+          size={32}
+        >
+          <Box as={MdAdd} color={theme.darkgray} size="100%" />
+        </Box>
+      </Box>
+    );
   }
 }
