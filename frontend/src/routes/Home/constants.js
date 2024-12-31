a75934ab1988c4ae46735f37a583469f45782d84
IveryChen <ivy_chen1@brown.edu>
2024-12-30 17:48:12 -0800

    add links to models

---
+++ frontend/src/routes/Home/constants.js
@@ -0,0 +1,5 @@
+export const HDRI =
+  "https://onegoodthing.s3.us-east-2.amazonaws.com/studio_small_05_4k.hdr";
+
+export const modelURL =
+  "https://onegoodthing.s3.us-east-2.amazonaws.com/jar.fbx";
