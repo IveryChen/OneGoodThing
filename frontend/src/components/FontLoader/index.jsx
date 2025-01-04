f9d0e7eaf53565029fd055737d7c05ad1b0d6c9c
IveryChen <ivy_chen1@brown.edu>
2025-01-04 12:44:47 -0800

    update fontloader links

--- frontend/src/components/FontLoader/index.jsx
+++ frontend/src/components/FontLoader/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { map } from "lodash";
 
 export default class FontLoader extends React.Component {
   constructor(props) {
@@ -9,19 +10,46 @@ export default class FontLoader extends React.Component {
   }
 
   componentDidMount() {
-    const interFont = new FontFace(
-      "Inter",
-      "url(https://fonts.gstatic.com/s/inter/v12/UcCO3FwrK3iLTeHuS_fvQtMwCp50KnMw2boKoduKmMEVuLyfAZ9hiA.woff2)",
-      { weight: "400" }
-    );
+    const fontWeights = [
+      {
+        weight: "300",
+        url: "https://fonts.gstatic.com/s/inter/v13/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7.woff2",
+      },
+      {
+        weight: "400",
+        url: "https://fonts.gstatic.com/s/inter/v13/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7.woff2",
+      },
+      {
+        weight: "500",
+        url: "https://fonts.gstatic.com/s/inter/v13/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2ZL7.woff2",
+      },
+      {
+        weight: "600",
+        url: "https://fonts.gstatic.com/s/inter/v13/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2pL7.woff2",
+      },
+      {
+        weight: "700",
+        url: "https://fonts.gstatic.com/s/inter/v13/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa25L7.woff2",
+      },
+    ];
 
-    interFont
-      .load()
-      .then((font) => {
-        document.fonts.add(font);
+    Promise.all(
+      map(fontWeights, ({ weight, url }) =>
+        new FontFace("Inter", `url(${url})`, { weight })
+          .load()
+          .then((font) => {
+            document.fonts.add(font);
+            return font;
+          })
+          .catch((err) => console.error(`Failed to load Inter ${weight}:`, err))
+      )
+    )
+      .then(() => {
         this.setState({ fontsLoaded: true });
       })
-      .catch((err) => console.error("Failed to load Inter font:", err));
+      .catch((err) => {
+        console.error("Error loading fonts:", err);
+      });
   }
 
   render() {
