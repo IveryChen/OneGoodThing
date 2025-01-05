9808bee55876d18041425241eef074cf94e929d5
IveryChen <ivy_chen1@brown.edu>
2025-01-05 10:58:13 -0800

    fill updateNote api call

--- frontend/src/api/updateNote.js
+++ frontend/src/api/updateNote.js
@@ -1,1 +1,15 @@
-export default async function updateNote() {}
+export default async function updateNote(noteId, text, token) {
+  const response = await fetch(`http://localhost:3000/api/notes/${noteId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ text }),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to update note");
+  }
+
+  return response.json();
+}
