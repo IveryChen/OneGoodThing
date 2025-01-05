a83daa01b6b427cf068d2343849860a857394eba
IveryChen <ivy_chen1@brown.edu>
2025-01-05 11:02:33 -0800

    keep state for isPending when submitting changes

--- frontend/src/routes/Notes/NoteModal.jsx
+++ frontend/src/routes/Notes/NoteModal.jsx
@@ -8,8 +8,6 @@ import IconButton from "../../components/IconButton";
 import { stickies, theme } from "../../constants/constants";
 import chooseColor from "../Notes/chooseColour";
 
-// import { AiOutlineEdit, AiTwotoneEdit } from "react-icons/ai";
-
 const StyledInput = styled.textarea`
   background-color: ${chooseColor()};
   border-style: none;
@@ -22,7 +20,7 @@ const StyledInput = styled.textarea`
 `;
 
 export default class NoteModal extends React.PureComponent {
-  state = { edit: false, note: "" };
+  state = { edit: false, isPending: false, note: "" };
 
   componentDidMount() {
     if (this.props.data) {
@@ -52,18 +50,22 @@ export default class NoteModal extends React.PureComponent {
     const { data, onNoteUpdated } = this.props;
     const { note } = this.state;
 
+    this.setState({ isPending: true });
+
     try {
       await updateNote(data._id, note, token);
       onNoteUpdated();
       this.props.onClose();
     } catch (e) {
       console.error(e);
+    } finally {
+      this.setState({ isPending: false });
     }
   };
 
   render() {
     const { data, isOpen, onClose } = this.props;
-    const { edit, note } = this.state;
+    const { edit, isPending, note } = this.state;
 
     if (!data) {
       return null;
@@ -83,6 +85,7 @@ export default class NoteModal extends React.PureComponent {
           <IconButton
             bg={edit ? theme.darkgray : "transparent"}
             color={edit ? theme.beige : theme.darkgray}
+            disabled={isPending}
             justifyItems="center"
             label={edit ? "SAVE" : "EDIT"}
             onClick={edit ? this.handleSubmit : this.toggleEdit}
