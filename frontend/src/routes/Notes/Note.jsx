78b4afa6d2a51f21a502404f914f84613029bd1f
IveryChen <ivy_chen1@brown.edu>
2025-01-04 19:30:44 -0800

    adjust styling for Note

--- frontend/src/routes/Notes/Note.jsx
+++ frontend/src/routes/Notes/Note.jsx
@@ -1,28 +1,43 @@
+import styled from "@emotion/styled";
 import React from "react";
 import { map } from "lodash";
 
 import Box from "../../components/Box";
 import Text from "../../components/Text";
-import { stickies } from "../../constants/constants";
+import { theme, stickies } from "../../constants/constants";
+
+const StyledBox = styled(Box)`
+  transition: all 0.3s ease;
+
+  &:hover {
+    box-shadow: -8px 8px ${theme.lightgray};
+    transform: translate(8px, -8px);
+  }
+`;
 
 export default class Note extends React.PureComponent {
   render() {
     const { data } = this.props;
 
     return (
-      <Box display="flex" flexWrap="wrap" gap={16} p={16}>
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+        gap={16}
+        p={16}
+      >
         {map(data, (note) => (
-          <Box
+          <StyledBox
+            aspectRatio={1}
             backgroundColor={stickies.lightyellow}
             key={note._id}
             p={16}
-            width={200}
           >
             <Text color="gray">{note.text}</Text>
             <Text color="gray" fontSize="sm">
               {new Date(note.createdAt).toLocaleDateString()}
             </Text>
-          </Box>
+          </StyledBox>
         ))}
       </Box>
     );
