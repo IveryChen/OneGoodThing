e4dc917c9d3c829f1ba6c2e7ed7520fa51c23062
IveryChen <ivy_chen1@brown.edu>
2025-01-04 19:14:48 -0800

    render note in Notes

--- frontend/src/routes/Notes/index.jsx
+++ frontend/src/routes/Notes/index.jsx
@@ -1,12 +1,45 @@
 import React from "react";
+import { map } from "lodash";
+import { Async } from "react-async";
 
-import IconButton from "../../components/IconButton";
+import { fetchNotes } from "../../api/fetchNotes";
+import Box from "../../components/Box";
+import Text from "../../components/Text";
 import { withRouter } from "../../utils/withRouter";
 
 class Notes extends React.PureComponent {
   render() {
-    return <IconButton borderStyle="none" label="Sign in" />;
+    const token = localStorage.getItem("token");
+
+    return (
+      <Async promiseFn={fetchNotes} token={token}>
+        {this.renderBody}
+      </Async>
+    );
   }
+  renderBody = ({ data: notes, error, isPending }) => {
+    if (isPending) return <Text>Loading...</Text>;
+    if (error) return <Text>Error: {error.message}</Text>;
+    if (notes) {
+      return (
+        <Box display="flex" flexWrap="wrap" gap={16} p={16}>
+          {map(notes, (note) => (
+            <Box
+              key={note._id}
+              backgroundColor="lightyellow"
+              p={16}
+              width={200}
+            >
+              <Text>{note.text}</Text>
+              <Text color="gray" fontSize="sm">
+                {new Date(note.createdAt).toLocaleDateString()}
+              </Text>
+            </Box>
+          ))}
+        </Box>
+      );
+    }
+  };
 }
 
 export default withRouter(Notes);
