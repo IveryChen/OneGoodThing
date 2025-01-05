62c1eb9b3ebbb2f7e1955a93fd0fe43d3dee9dc5
IveryChen <ivy_chen1@brown.edu>
2025-01-04 19:22:52 -0800

    render Note in Notes

--- frontend/src/routes/Notes/index.jsx
+++ frontend/src/routes/Notes/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { map } from "lodash";
 import { Async } from "react-async";
 
 import { fetchNotes } from "../../api/fetchNotes";
-import Box from "../../components/Box";
 import Text from "../../components/Text";
 import { withRouter } from "../../utils/withRouter";
+
+import Note from "./Note";
 
 class Notes extends React.PureComponent {
   render() {
@@ -18,26 +18,15 @@ class Notes extends React.PureComponent {
     );
   }
   renderBody = ({ data: notes, error, isPending }) => {
-    if (isPending) return <Text>Loading...</Text>;
-    if (error) return <Text>Error: {error.message}</Text>;
-    if (notes) {
+    if (isPending)
       return (
-        <Box display="flex" flexWrap="wrap" gap={16} p={16}>
-          {map(notes, (note) => (
-            <Box
-              key={note._id}
-              backgroundColor="lightyellow"
-              p={16}
-              width={200}
-            >
-              <Text>{note.text}</Text>
-              <Text color="gray" fontSize="sm">
-                {new Date(note.createdAt).toLocaleDateString()}
-              </Text>
-            </Box>
-          ))}
-        </Box>
+        <Text color="gray" p={16}>
+          Loading...
+        </Text>
       );
+    if (error) return <Text p={16}>Error: {error.message}</Text>;
+    if (notes) {
+      return <Note data={notes} />;
     }
   };
 }
